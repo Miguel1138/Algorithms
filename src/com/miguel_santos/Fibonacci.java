6375bf864f4da6e24795eb2017626aeb1fd59842
Miguel S. da Silva <miguels.dasilva00@gmail.com>
2021-10-09 19:56:34 -0300

    Add gitignore file
    - Also delete duplicate classes.

--- src/com/miguel_santos/Fibonacci.java
+++
@@ -1,8 +0,0 @@
-package com.miguel_santos;
-
-public class Fibonacci {
-    public static int fib(int n) {
-        // Fibonacci Fn = Fn-1 + Fn-2
-        return (n > 1) ? fib(n - 1) + fib(n - 2) : n;
-    }
-}
